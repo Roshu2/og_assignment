983bc4de473d4ea1ec5550e7a071446648f69b9a
Roshu2 <kyumin1020@gmail.com>
2022-09-10 18:01:32 +0900

    FIX - customadmin app to ogadmin

--- ogadmin/apps.py
+++ ogadmin/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CustomadminConfig(AppConfig):
+class OgadminConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customadmin'
+    name = 'ogadmin'
